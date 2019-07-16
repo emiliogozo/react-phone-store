d18ddce28ffd0a25a7e59ce65e5db22d749fb279
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2019-07-16 23:33:29 +0800

    feat: handleDetail and addToCart method

--- src/context.js
+++ src/context.js
@@ -6,7 +6,8 @@ const ProductContext = React.createContext()
 class ProductProvider extends Component {
   state = {
     products: [],
-    detailProduct
+    detailProduct,
+    cart: []
   }
 
   componentDidMount() {
@@ -22,12 +23,26 @@ class ProductProvider extends Component {
     this.setState({ products })
   }
 
-  handleDetail = () => {
-    console.log('hello from detail')
+  getItem = id => this.state.products.find(item => item.id === id)
+
+  handleDetail = id => {
+    const detailProduct = this.getItem(id)
+    this.setState({ detailProduct })
   }
 
-  addToCart = () => {
-    console.log('hello from add to cart')
+  addToCart = id => {
+    let tempProducts = [...this.state.products]
+    const index = tempProducts.indexOf(this.getItem(id))
+    const product = tempProducts[index]
+    product.inCart = true
+    product.count = 1
+    const price = product.price
+    product.total = price
+
+    this.setState({
+      products: tempProducts,
+      cart: [...this.state.cart, product]
+    })
   }
 
   render() {
